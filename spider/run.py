5144c09dca75f294ec04f6ccbf5c5e863dd7376f
hexiaosong <he20093334@163.com>
2017-08-29 11:26:46 +0800

    chg: 爬虫添加多线程

--- spider/run.py
+++ spider/run.py
@@ -7,21 +7,34 @@ import requests
 import threading
 import urllib
 x = 0
-#run函数
-def Run():
+def Run(url):
+    html = requests.get(url[0]).text
+    urllist = getUrl.delay(html).get()
+    for url in urllist:
+        url = 'http://umei.cc'+url
+        print url
+        ImgHtml = requests.get(url).text
+        imglist = getImg.delay(ImgHtml).get()
+        for imgurl in imglist: 
+            print imgurl
+            urllib.urlretrieve(imgurl,'/test/%s.jpg' % x)  
+            global x
+            x += 1
+#加入多线程
+def main():
     pageurls = getPageUrl.delay().get()
-    for i in range(1,len(pageurls)):
-        html = requests.get(pageurls[i]).text
-        urllist = getUrl.delay(html).get()
-        for url in urllist:
-            url = 'http://umei.cc'+url
-            print url
-            ImgHtml = requests.get(url).text
-            imglist = getImg.delay(ImgHtml).get()
-            for imgurl in imglist: 
-                print imgurl
-                urllib.urlretrieve(imgurl,'/Users/hexiaosong/github/celeryTry/spider/test/%s.jpg' % x)  
-                global x
-                x += 1
+    urlgroup = []
+    j = 0
+    for i in range(0,len(pageurls)):
+        if (j + 1) % 2 == 0:
+            urlgroup.append(pageurls[i])
+            t = threading.Thread(target=Run,args=(urlgroup,))
+            t.start()
+            t.join(1)
+            urlgroup = []
+        else:
+            urlgroup.append(pageurls[i])
+        j = j + 1
+
 if __name__ == '__main__':
-    Run()
+    main()
